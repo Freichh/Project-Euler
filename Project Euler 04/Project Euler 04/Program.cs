8399f84fcdfbc2bcb2f89f74361967a43ac4f217
Freek Heining <freek.heining@gmail.com>
2020-06-24 20:45:16 +0200

    Finished 4 & 5

---
+++ Project Euler 04/Project Euler 04/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project_Euler_04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// A palindromic number reads the same both ways. 
+            /// The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+            /// Find the largest palindrome made from the product of two 3 - digit numbers.
+
+            /// c = a * b
+            /// a = 100 t/m 999
+            /// b = 100 t/m 999
+            /// Begin van hoog naar laag
+            /// verlaag a en b steeds totdat c == c reverse
+            /// Sla op en vergelijk uitkomsten. Kies de hoogste.
+
+            //int b = 999;
+            int c;
+            int cReversed;
+            List<int> palindromes = new List<int>();
+
+            for (int b = 999; b > 100; b--)
+            {
+                for (int i = 999; i > 100; i--)
+                {
+                    int a = i;
+                    c = a * b;
+                    cReversed = reverse(c);
+
+                    if (c == cReversed)
+                    {
+                        palindromes.Add(cReversed);
+                    }
+                }
+            }
+            Console.WriteLine(palindromes.Max());
+
+
+        }
+
+        public static int reverse(int x)
+        {
+            int rev = 0;
+            while (x != 0)
+            {
+                rev = rev * 10 + x % 10;
+                x = x / 10;
+            }
+
+            return rev;
+        }
+
+    }
+}
---
+++ Project Euler 05/Project Euler 05/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Project_Euler_05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
+            // What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20 ?
+
+            // Begin vanaf x = 2522. 
+            // Alleen even getallen
+            // Check eerst of mod 20 == 0;
+            // Loop modulus 1 t/m 20
+            // Flag false als x % != 0 en stop, of ga verder als niet 0
+            // Als loop klaar is en flag true, sla op en eindig
+
+            int x = 20;
+            int result;
+            bool y = true;
+
+
+            for (int i = 20; i > 1; i--)
+            {
+                result = x % i;
+
+                if (result != 0)
+                {
+                    x = x + 20;
+                    i = 21;
+                }
+
+            }
+            Console.WriteLine("!!!!!! x =  " + x);
+
+
+
+        }
+    }
+}
